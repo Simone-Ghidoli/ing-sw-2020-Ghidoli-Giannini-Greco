3ddd482fade6cbc27040bb9983a0aef528a11f8a
Nicog97 <10568199@polimi.it>
2020-03-28 17:34:49 +0100

    start game

--- src/main/java/it/polimi/ingsw/ps60/serverSide/controller/StartGame.java
+++ src/main/java/it/polimi/ingsw/ps60/serverSide/controller/StartGame.java
@@ -1,10 +1,46 @@
 package it.polimi.ingsw.ps60.serverSide.controller;
 
 
-public class StartGame {
+import it.polimi.ingsw.ps60.GlobalVariables;
+import it.polimi.ingsw.ps60.serverSide.model.Board;
+import it.polimi.ingsw.ps60.serverSide.model.Player;
 
-    public StartGame(String[] nicknames){
+public class StartGame {
+    public StartGame( String[] nicknames){
+
+        startBoard(nicknames);
+        giveDivinityCard();
+        selectDivinityCard();
+        posizionamentoPedine();
 
     }
 
+    public void checkFirstPlayer(){
+
+    }
+    public void createNickname(){
+
+    }
+    public void startBoard(String[] nickname){
+        Board board = new Board(nickname);
+        GlobalVariables.game=board;
+    }
+    public void giveDivinityCard(){
+
+
+
+
+
+    }
+    public void selectDivinityCard(){
+        for(int j=0; j<GlobalVariables.game.getPlayersNumber(); j++)
+            GlobalVariables.game.getPlayerById(GlobalVariables.IdPlayer.getPlayerByInt(j)).setDivinityCard( );
+
+
+    }
+    public void posizionamentoPedine() {
+
+
+
+    }
 }
