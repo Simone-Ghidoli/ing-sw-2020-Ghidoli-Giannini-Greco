e83920e13c2119e10fb0a8a189d8bf6bf190c642
Nicog97 <10568199@polimi.it>
2020-03-30 16:21:55 +0200

    add Zeus and Triton strategy

--- src/main/java/it/polimi/ingsw/ps60/serverSide/controller/turnStrategy/AthenaStrategy.java
+++ src/main/java/it/polimi/ingsw/ps60/serverSide/controller/turnStrategy/AthenaStrategy.java
@@ -2,11 +2,7 @@ package it.polimi.ingsw.ps60.serverSide.controller.turnStrategy;
 
 import it.polimi.ingsw.ps60.GlobalVariables;
 
-/**
- * Athena's class just switch the control bit of the god. (true->false or false->true).
- * Always set to false at the start of the turn and eventually set to true if one of the workers level up.
- * Standard move and build.
- */
+
 public class AthenaStrategy extends TurnStrategy {
     public void ChangeBit(){
         GlobalVariables.DivinityCard.ATHENA.setBitException(false);
---
+++ src/main/java/it/polimi/ingsw/ps60/serverSide/controller/turnStrategy/TritonStrategy.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.ps60.serverSide.controller.turnStrategy;
+
+import java.util.List;
+
+public class TritonStrategy extends TurnStrategy {
+    @Override
+    public List<int[]>[] baseMovement() {
+        List<int[]>[] position = super.baseMovement();
+        for (int k = 0; k < 2; k++) {
+            for (int i = 0; i < position.length; i++) {
+                for (int j = 0; j < 2; j++) {
+                    if (position[k].get(i)[j] == 0 || position[k].get(i)[j] == 4) {
+                        position=super.baseMovement();
+                    }
+                }
+
+            }
+        }
+        return position;
+    }
+}
