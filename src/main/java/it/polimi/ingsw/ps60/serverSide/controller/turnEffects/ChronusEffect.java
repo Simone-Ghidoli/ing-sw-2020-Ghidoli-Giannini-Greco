ea61fd0b8c05a66b89fe89d4ae176fc3e3936ddb
Simone <10561056@polimi.it>
2020-03-30 16:26:18 +0200

    Implemented Atlas

---
+++ src/main/java/it/polimi/ingsw/ps60/serverSide/controller/turnEffects/ChronusEffect.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.ps60.serverSide.controller.turnEffects;
+
+public class ChronusEffect extends Turn {
+
+    @Override
+    public void winConditions() {
+        super.winConditions();
+        
+    }
+}
