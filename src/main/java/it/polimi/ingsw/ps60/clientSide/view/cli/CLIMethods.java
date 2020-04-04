a2b6a29a9eee10790cc54a30b3ae7c3c7609a969
Vincenzo Greco <grecovincenzo98@gmail.com>
2020-04-04 02:20:55 +0200

    Build and move CLI implementation

--- src/main/java/it/polimi/ingsw/ps60/clientSide/view/cli/CLIMethods.java
+++ src/main/java/it/polimi/ingsw/ps60/clientSide/view/cli/CLIMethods.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.ps60.clientSide.view.cli;
 
 import it.polimi.ingsw.ps60.GlobalVariables;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class CLIMethods {
 
     /* questi sono ascii da 48 a 64
@@ -25,7 +28,6 @@ public class CLIMethods {
     @       3       3
      */
     public static void printBoard(char[] board) {
-        GlobalVariables.Colour colour;
 
         System.out.println("Legend:");
         System.out.println("The number indicates the building level (4 is the dome), the colour indicates the player");
@@ -57,6 +59,63 @@ public class CLIMethods {
             System.out.print("  ");
 
 
+        }
+    }
+
+    public static int printPossibleMoves(List<int[]>[] moves){
+        int choice = 0;
+        for (int i = 0; i < moves.length; i++){
+            System.out.println("Available choice for worker " + (i+1));
+            for(int j = 0; j < moves[i].size(); j++){
+                System.out.println("Press " + (choice + 1) + " in order to move in the cell: " +
+                        (moves[i].get(j)[0] + 1) + "; " + (moves[i].get(j)[1] + 1));
+                choice++;
+            }
+        }
+
+        return choice;
+    }
+
+    public static int[] moveChoice(List<int[]>[] moves) {
+        System.out.println("Select where to move");
+        int i = printPossibleMoves(moves);
+        int choice = new Scanner(System.in).nextInt();
+
+        if (choice > i) {
+            System.out.println("Wrong input");
+            return moveChoice(moves);
+        } else {
+            int j = 0;
+            while (moves[j].size() < choice) {
+                choice = choice - moves[j].size();
+                j++;
+            }
+            return moves[j].get(choice - 1);
+        }
+    }
+
+    public static int printPossibleBuilds(List<int[]> moves){
+        int choice = 0;
+
+        for (int i = 0; i < moves.size(); i++){
+            System.out.println("Press " + (choice + 1) + " in order to build on the cell: " +
+                    (moves.get(i)[0] + 1) + "; " + (moves.get(i)[1] + 1));
+            choice++;
+        }
+        return choice;
+    }
+
+    public static int[] buildChoice(List<int[]> moves){
+        System.out.println("Select where to build");
+        int i = printPossibleBuilds(moves);
+        int choice = new Scanner(System.in).nextInt();
+
+        if (choice > i){
+            System.out.println("Wrong input");
+            return buildChoice(moves);
+        }
+        else {
+            return moves.get(choice-1);
         }
     }
 }
--- src/main/java/it/polimi/ingsw/ps60/clientSide/view/cli/CLIStarter.java
+++ src/main/java/it/polimi/ingsw/ps60/clientSide/view/cli/CLIStarter.java
@@ -1,8 +1,5 @@
 package it.polimi.ingsw.ps60.clientSide.view.cli;
 
-
-
-
 public class CLIStarter {
     public static void main(String[] args) {
         char[] chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@',
