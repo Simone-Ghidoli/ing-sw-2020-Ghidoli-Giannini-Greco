4d7759d1a68e61c69ab81d877355d6d3fc99cb1f
Nicog97 <10568199@polimi.it>
2020-04-05 16:08:30 +0200

    PlayerTest add

--- src/test/java/it/polimi/ingsw/ps60/serverSide/model/WorkerTest.java
+++ src/test/java/it/polimi/ingsw/ps60/serverSide/model/WorkerTest.java
@@ -1,4 +1,10 @@
 package it.polimi.ingsw.ps60.serverSide.model;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class WorkerTest {
 }
