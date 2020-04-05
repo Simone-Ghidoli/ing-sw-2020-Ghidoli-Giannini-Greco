ee81bffdce35ec3a0d2339888acc225e5a8e5782
Simone <10561056@polimi.it>
2020-04-05 17:45:14 +0200

    implemented ClientStarter/Reader

--- src/main/java/it/polimi/ingsw/ps60/clientSide/view/client/ClientReader.java
+++ src/main/java/it/polimi/ingsw/ps60/clientSide/view/client/ClientReader.java
@@ -1,11 +1,37 @@
 package it.polimi.ingsw.ps60.clientSide.view.client;
 
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientReader extends Thread {
+    List<String> messagesFromServer;
     Socket socket;
+    String serverSays;
+    BufferedReader br;
+    InputStream in;
+    PrintWriter pr;
 
     public ClientReader(Socket sock){
         socket=sock;
+        messagesFromServer=new ArrayList<>();
+    }
+
+    public void run(){
+        try {
+            while(true) {
+                br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                serverSays = br.readLine();
+                synchronized (messagesFromServer) {
+                    messagesFromServer.add(serverSays);
+                }
+            }
+        }
+        catch(IOException e){
+            //todo chiamo la disconnessione e la chiusura della partita
+        }
     }
 }
